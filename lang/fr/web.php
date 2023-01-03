67b31436858a79bd1489e2c6cbb62dd6592890ea
Julien Machiels <julien.machiels@protonmail.com>
2023-01-03 19:06:11 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: Kanojo/Website
    Translate-URL: https://hosted.weblate.org/projects/kanojo/website/fr/

--- lang/fr/web.php
+++ lang/fr/web.php
@@ -1,31 +1,41 @@
 <?php
-
 return [
-    "about" => [
-        "title" => "Kanojo est une base de données {0} de films pour adultes japonais et de vidéos de gravure.",
-        "community" => "communautaire",
-        "subtitle" => "Chaque élément de données est soumis et sélectionné par nos utilisateurs,{0} dans le but de fournir des informations de haute qualité.",
-        "point1" => "Une vaste base de données d'informations sur les films, les studios et les modèles, avec images. Elle comprend tout ce dont vous avez besoin pour trouver votre prochain titre préféré, d'une liste du contenu aux informations détaillées sur les modèles.",
-        "point2" => "Plus besoin d'extraire les informations des boutiques en ligne ou de contourner les bans d'IP pour obtenir les informations dont vous avez besoin. Nous fournissons une API complète et facile à utiliser pour les développeurs, afin d'intégrer nos données à votre outil ou service.",
-        "point3" => "Développé par des gens comme vous. Nous avons créé Kanojo pour résoudre certains des problèmes que nous avons rencontrés avec d'autres sites et outils au cours des 10 dernières années.",
+    'about' => [
+        'title' => "Kanojo est une base de données {0} de films pour adultes japonais et de vidéos de gravure",
+        'community' => "communautaire",
+        'subtitle' => "Chaque élément de données est soumis et sélectionné par nos utilisateurs,{0} dans le but de fournir des informations de haute qualité.",
+        'point1' => "Une vaste base de données d'informations sur les films, les studios et les modèles, avec images. Elle comprend tout ce dont vous avez besoin pour trouver votre prochain titre préféré, d'une liste du contenu aux informations détaillées sur les modèles.",
+        'point2' => "Plus besoin d'extraire les informations des boutiques en ligne ou de contourner les bans d'IP pour obtenir les informations dont vous avez besoin. Nous fournissons une API complète et facile à utiliser pour les développeurs, afin d'intégrer nos données à votre outil ou service.",
+        'point3' => "Développé par des gens comme vous. Nous avons créé Kanojo pour résoudre certains des problèmes que nous avons rencontrés avec d'autres sites et outils au cours des 10 dernières années.",
     ],
-    "general" => [
-        "search" => "Rechercher",
-        "unknown" => "Inconnu",
-        "present" => "Présent",
+    'general' => [
+        'search' => "Rechercher",
+        'unknown' => "Inconnu",
+        'present' => "Présent",
+        'save_changes' => "Enregistrer",
     ],
-    "personShow" => [
-        "moviesCount" => "{number} films",
+    'personShow' => [
+        'moviesCount' => "{number} films",
     ],
-    "welcome" => [
-        "slogan" => "Trouvez votre prochain coup de coeur",
-        "countMovies" => "Films",
-        "countModels" => "Modèles",
-        "countCategories" => "Catégories",
-        "recentlyReleasedMovies" => "Films récemment sortis",
-        "popularModels" => "Modèles populaires",
-        "popularMovies" => "Films populaires",
-        "recentlyAddedMovies" => "Films récemment ajoutés",
-        "recentlyUpdatedMovies" => "Films récemment mis à jour",
-    ]
+    'welcome' => [
+        'slogan' => "Trouvez votre prochain coup de coeur",
+        'countMovies' => "Films",
+        'countModels' => "Modèles",
+        'countCategories' => "Catégories",
+        'recentlyReleasedMovies' => "Films récemment sortis",
+        'popularModels' => "Modèles populaires",
+        'popularMovies' => "Films populaires",
+        'recentlyAddedMovies' => "Films récemment ajoutés",
+        'recentlyUpdatedMovies' => "Films récemment mis à jour",
+    ],
+    'settings' => [
+        'account' => [
+            'content_visibility' => "Visibilité du contenu",
+            'title' => "Paramètres du compte",
+            'show_adult_content' => "Voir le contenu pornographique",
+            'show_vr_content' => "Voir le contenu en réalité virtuelle",
+            'show_gravure_content' => "Voir le contenu de gravure",
+            'show_gravure_minors_content' => "Voir le contenu de gravure mettant en scène des mineurs",
+        ],
+    ],
 ];
