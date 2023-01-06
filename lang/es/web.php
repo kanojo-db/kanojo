38eb3aae0e123a99bd00cbdb213b34b3d07b0767
Allen Verdín <averdintf@gmail.com>
2023-01-06 17:43:59 +0000

    fix(i18n): update translations for Spanish

--- lang/es/web.php
+++ lang/es/web.php
@@ -1,1 +1,58 @@
 <?php
+return [
+    'about' => [
+        'community' => "Comunidad",
+        'point3' => "Construido por personas como tú. Creamos Kanojo para resolver algunos de los problemas que hemos tenido con otros sitios o herramientas en los últimos 10 años.",
+        'title' => "Kanojo es una base de datos {0} de videos japonese para adultos y videos gravure",
+        'subtitle' => "Todos los datos son enviados y seleccionados por los usuarios,{0} con el objetivo de proveer una información de alta calidad.",
+        'point2' => "Se acabo el screpear tiendas en línea o evadir baneos de IP para obtener la información que necesitas. Nosotros proveemos un API comprensivo y fácil de usar para los desarrolladores, para integrar nuestros datos con tus herramientas o servicios.",
+        'point1' => "Una extensa base de datos con información de películas, estudios y modelos, complementada con imágenes. Incluye todo lo que necesitas para encontrar tu siguiente título favorito, desde etiquetas detalladas hasta información de la modelo.",
+    ],
+    'dialogs' => [
+        'create_token' => [
+            'title' => "Crear Token de API",
+            'description' => "Ingresa un nombre único al token del API para ayudarte a identificarlo después.",
+        ],
+    ],
+    'general' => [
+        'search' => "Buscar",
+        'present' => "Presente",
+        'save_changes' => "Guardar Cambios",
+        'unknown' => "Desconocido",
+    ],
+    'personShow' => [
+        'moviesCount' => "{number} Películas",
+    ],
+    'welcome' => [
+        'countMovies' => "Películas",
+        'countCategories' => "Categorías",
+        'recentlyReleasedMovies' => "Películas recientemente lanzadas",
+        'popularModels' => "Modelos populares",
+        'popularMovies' => "Películas populares",
+        'recentlyAddedMovies' => "Películas recientemente añadidas",
+        'slogan' => "Encuentra tu siguiente amor",
+        'recentlyUpdatedMovies' => "Películas recientemente actualizadas",
+        'countModels' => "Modelos",
+    ],
+    'settings' => [
+        'account' => [
+            'title' => "Configuración de cuenta",
+            'content_visibility' => "Visibilidad de contenido",
+            'show_adult_content' => "Mostrar contenido adulto",
+            'show_vr_content' => "Mostrar contenido con RV",
+            'show_gravure_content' => "Mostrar contenido gravure",
+            'show_gravure_minors_content' => "Mostrar contenido gravure con menores",
+            'content_visibility_tooltip' => "Selecciona que contenido quieres ver u ocultar en el sito.",
+        ],
+        'sessions' => [
+            'title' => "Sesiones",
+        ],
+        'tokens' => [
+            'title' => "Tokens de API",
+            'create_token' => "Crear Token de API",
+            'success' => "Tu token de API a sido creado. Por favor cópialo, ya que no volverá a mostrarse.",
+            'copy_token' => "Token copiado",
+            'no_tokens' => "No tienes tokens de API, para acceder al API o usar las herramientas que requieren un token de API, necesitas crear uno.",
+        ],
+    ],
+];
