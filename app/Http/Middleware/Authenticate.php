0920a3e7f2c5199c8e6b2cf7aad782dc5a10532e
MrTimscampi <iamtimscampi@gmail.com>
2023-02-10 14:16:15 +0100

    fix(middleware): incompatible method in Authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,8 +11,11 @@ class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request)
     {
         return $request->expectsJson() ? null : route('login');
     }
