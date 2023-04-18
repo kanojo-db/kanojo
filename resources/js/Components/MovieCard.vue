debf4606b00c2088739a739bd275548617e65194
MrTimscampi <iamtimscampi@gmail.com>
2023-04-18 22:51:48 +0200

    fix: use ID if slug does not exist
    This will not work for routing, but it at least fixes the SSR crash.

--- resources/js/Components/MovieCard.vue
+++ resources/js/Components/MovieCard.vue
@@ -53,7 +53,7 @@ const userScore = computed(() => {
 <template>
     <Link
         class="block"
-        :href="route('movies.show', { movie: movie.slug })"
+        :href="route('movies.show', { movie: movie.slug ?? movie.id })"
         style="width: 200px"
     >
         <div class="fit column no-wrap justify-start items-start content-start">
