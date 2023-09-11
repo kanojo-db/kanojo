fc15c8972bb5c8cf968817c515f4c8b05f343366
MrTimscampi <iamtimscampi@gmail.com>
2023-09-11 21:49:00 +0200

    Revert "fix: use full height for all elements on previews"
    This reverts commit eceb5231aacea10d3ed0e6122c6cacdd7d8f03f9.

--- routes/api.php
+++ routes/api.php
@@ -41,59 +41,59 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
  */
-// Route::middleware('auth:sanctum')->group(function () {
-/**
- * Account
- */
-Route::get('/account/{account_id}', [UserDetailsController::class, 'show'])->where('account_id', '\d+');
-Route::get('/account/{account_id}/favorite/movies', [UserFavoritesController::class, 'show'])->where('account_id', '\d+');
-Route::get('/account/{account_id}/wishlist/movies', [UserWishlistController::class, 'show'])->where('account_id', '\d+');
-Route::get('/account/{account_id}/collection/movies', [UserCollectionController::class, 'show'])->where('account_id', '\d+');
+Route::middleware('auth:sanctum')->group(function () {
+    /**
+     * Account
+     */
+    Route::get('/account/{account_id}', [UserDetailsController::class, 'show'])->where('account_id', '\d+');
+    Route::get('/account/{account_id}/favorite/movies', [UserFavoritesController::class, 'show'])->where('account_id', '\d+');
+    Route::get('/account/{account_id}/wishlist/movies', [UserWishlistController::class, 'show'])->where('account_id', '\d+');
+    Route::get('/account/{account_id}/collection/movies', [UserCollectionController::class, 'show'])->where('account_id', '\d+');
 
-/**
- * Changes
- */
-Route::get('/movie/changes', [ChangesMoviesController::class, 'index']);
-Route::get('/person/changes', [ChangesPeopleController::class, 'index']);
-Route::get('/studio/changes', [ChangesStudioController::class, 'index']);
-Route::get('/series/changes', [ChangesSeriesController::class, 'index']);
+    /**
+     * Changes
+     */
+    Route::get('/movie/changes', [ChangesMoviesController::class, 'index']);
+    Route::get('/person/changes', [ChangesPeopleController::class, 'index']);
+    Route::get('/studio/changes', [ChangesStudioController::class, 'index']);
+    Route::get('/series/changes', [ChangesSeriesController::class, 'index']);
 
-/**
- * Search
- */
-Route::get('/search/movie', [SearchMovieController::class, 'index']);
-Route::get('/search/person', [SearchPersonController::class, 'index']);
-Route::get('/search/studio', [SearchStudioController::class, 'index']);
-Route::get('/search/series', [SearchSeriesController::class, 'index']);
+    /**
+     * Search
+     */
+    Route::get('/search/movie', [SearchMovieController::class, 'index']);
+    Route::get('/search/person', [SearchPersonController::class, 'index']);
+    Route::get('/search/studio', [SearchStudioController::class, 'index']);
+    Route::get('/search/series', [SearchSeriesController::class, 'index']);
 
-/**
- * Movie
- */
-Route::get('/movie/popular', [MoviePopularController::class, 'index']);
-Route::get('/movie/recently-released', [MovieRecentlyReleasedController::class, 'index']);
-Route::get('/movie/{movie_id}', [MovieDetailsController::class, 'show'])->where('movie_id', '\d+');
-Route::get('/movie/{movie_id}/credits', [MovieCreditsController::class, 'show'])->where('movie_id', '\d+');
-Route::get('/movie/{movie_id}/images', [MovieMediaController::class, 'show'])->where('movie_id', '\d+');
+    /**
+     * Movie
+     */
+    Route::get('/movie/popular', [MoviePopularController::class, 'index']);
+    Route::get('/movie/recently-released', [MovieRecentlyReleasedController::class, 'index']);
+    Route::get('/movie/{movie_id}', [MovieDetailsController::class, 'show'])->where('movie_id', '\d+');
+    Route::get('/movie/{movie_id}/credits', [MovieCreditsController::class, 'show'])->where('movie_id', '\d+');
+    Route::get('/movie/{movie_id}/images', [MovieMediaController::class, 'show'])->where('movie_id', '\d+');
 
-/**
- * Person
- */
-Route::get('/person/popular', [PersonPopularController::class, 'index']);
-Route::get('/person/{person_id}', [PersonDetailsController::class, 'show'])->where('person_id', '\d+');
-Route::get('/person/{person_id}/images', [PersonMediaController::class, 'show'])->where('person_id', '\d+');
-Route::get('/person/{person_id}/movies', [PersonMoviesController::class, 'show'])->where('person_id', '\d+');
+    /**
+     * Person
+     */
+    Route::get('/person/popular', [PersonPopularController::class, 'index']);
+    Route::get('/person/{person_id}', [PersonDetailsController::class, 'show'])->where('person_id', '\d+');
+    Route::get('/person/{person_id}/images', [PersonMediaController::class, 'show'])->where('person_id', '\d+');
+    Route::get('/person/{person_id}/movies', [PersonMoviesController::class, 'show'])->where('person_id', '\d+');
 
-/**
- * Studio
- */
-Route::get('/studio/{studio_id}', [StudioDetailsController::class, 'show'])->where('studio_id', '\d+');
-Route::get('/studio/{studio_id}/images', [StudioMediaController::class, 'show'])->where('studio_id', '\d+');
-Route::get('/studio/{studio_id}/movies', [StudioMoviesController::class, 'show'])->where('studio_id', '\d+');
+    /**
+     * Studio
+     */
+    Route::get('/studio/{studio_id}', [StudioDetailsController::class, 'show'])->where('studio_id', '\d+');
+    Route::get('/studio/{studio_id}/images', [StudioMediaController::class, 'show'])->where('studio_id', '\d+');
+    Route::get('/studio/{studio_id}/movies', [StudioMoviesController::class, 'show'])->where('studio_id', '\d+');
 
-/**
- * Series
- */
-Route::get('/series/{series_id}', [SeriesDetailsController::class, 'show'])->where('series_id', '\d+');
-Route::get('/series/{series_id}/images', [SeriesMediaController::class, 'show'])->where('series_id', '\d+');
-Route::get('/series/{series_id}/movies', [SeriesMoviesController::class, 'show'])->where('series_id', '\d+');
-// });
+    /**
+     * Series
+     */
+    Route::get('/series/{series_id}', [SeriesDetailsController::class, 'show'])->where('series_id', '\d+');
+    Route::get('/series/{series_id}/images', [SeriesMediaController::class, 'show'])->where('series_id', '\d+');
+    Route::get('/series/{series_id}/movies', [SeriesMoviesController::class, 'show'])->where('series_id', '\d+');
+});
