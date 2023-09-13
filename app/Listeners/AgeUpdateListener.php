abb2d62aaceb16c1f58056e72801a551b14b04ad
MrTimscampi <iamtimscampi@gmail.com>
2023-09-13 22:14:02 +0200

    fix(events): non-existing event dispatched for age updates

--- app/Listeners/AgeUpdateListener.php
+++ app/Listeners/AgeUpdateListener.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace App\Listeners;
 
-use App\Console\Commands\ComputeModelsAges;
 use App\Events\MovieUpdated;
 use App\Events\PersonUpdated;
+use App\Jobs\ComputeModelsAgesJob;
+use Illuminate\Events\Dispatcher;
 
 class AgeUpdateListener
 {
     /**
      * Handle MovieUpdated events.
      */
-    public function onMovieUpdate(MovieUpdated $event)
+    public function onMovieUpdate(MovieUpdated $event): void
     {
         // Check if we have models on the movie first, to avoid unnecessary jobs
         if ($event->movie->models()->count() === 0) {
@@ -21,13 +22,13 @@ class AgeUpdateListener
         }
 
         // Queue a job to update the age of the models for this movie
-        ComputeModelsAges::dispatch($event->movie);
+        ComputeModelsAgesJob::dispatch($event->movie);
     }
 
     /**
      * Handle PersonUpdated events.
      */
-    public function onPersonUpdate(PersonUpdated $event)
+    public function onPersonUpdate(PersonUpdated $event): void
     {
         // If the movies relationship is not loaded, load it
         if (! $event->person->relationLoaded('movies')) {
@@ -36,16 +37,14 @@ class AgeUpdateListener
 
         // Queue a job to update the age of the models for each movie
         foreach ($event->person->movies as $movie) {
-            ComputeModelsAges::dispatch($movie);
+            ComputeModelsAgesJob::dispatch($movie);
         }
     }
 
     /**
      * Register the listeners for the subscriber.
-     *
-     * @param  Illuminate\Events\Dispatcher  $events
      */
-    public function subscribe($events)
+    public function subscribe(Dispatcher $events): void
     {
         $events->listen(
             'App\Events\MovieUpdated',
