1333e50edbce8196f3974ab427aedcadd50bf39e
MrTimscampi <iamtimscampi@gmail.com>
2023-09-18 17:39:34 +0200

    fix: remove IP and user agent from change histories

--- config/audit.php
+++ config/audit.php
@@ -45,8 +45,6 @@ return [
     |
      */
     'resolvers' => [
-        'ip_address' => OwenIt\Auditing\Resolvers\IpAddressResolver::class,
-        'user_agent' => OwenIt\Auditing\Resolvers\UserAgentResolver::class,
         'url' => OwenIt\Auditing\Resolvers\UrlResolver::class,
     ],
 
---
+++ database/migrations/2023_09_14_124430_remove_ip_address_and_user_agent_from_audits.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('audits', function (Blueprint $table) {
+            $table->dropColumn(['ip_address', 'user_agent']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('audits', function (Blueprint $table) {
+            $table->string('ip_address', 45)->nullable();
+            $table->string('user_agent', 1023)->nullable();
+        });
+    }
+};
