0957ba83d07cde8b3d8a366a94f00f1d9dc4076d
MrTimscampi <iamtimscampi@gmail.com>
2023-09-22 18:58:00 +0200

    test: switch coverage report to v8
    Istanbul is currently causing test suites to fail, so we're using v8 for now.

--- vite.config.js
+++ vite.config.js
@@ -48,6 +48,9 @@ export default defineConfig(({ mode, ssrBuild }) => {
                     inline: ['vuetify'],
                 },
             },
+            coverage: {
+                reporter: ['lcov'],
+            },
         },
         plugins: [
             laravel({
